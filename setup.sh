d7507e4aa0898dcf971075837e87095c1c283758
Muhannad <zero9007@gmail.com>
2019-12-06 11:02:01 +0500

    fixes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 source venv/bin/activate
-while true; do
-    flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Db Upgrade failed, retrying in 5 seconds...
-    sleep 5
-done
+if [ "$DATABASE" = "mysql" ]
+then
+    echo "Waiting for database..."
+    echo "Db started"
+fi
+flask db upgrade
 exec gunicorn -b :5000 --access-logfile - --error-logfile - krunchly:app
