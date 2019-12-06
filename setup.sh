2ee48818b9af8250a8f3347ceb29ead54d7304cd
Muhannad <zero9007@gmail.com>
2019-12-06 10:27:10 +0500

    added docker image support for pymysql

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 source venv/bin/activate
-flask db upgrade
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0"]]; then
+        break
+    fi
+    echo Db Upgrade failed, retrying in 5 seconds...
+    sleep 5
+done
 exec gunicorn -b :5000 --access-logfile - --error-logfile - krunchly:app
